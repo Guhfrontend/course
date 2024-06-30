14043d153f7289619413f0d86ac57f18dad458f5
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-06-30 19:16:22 -0300

    Service e registro de componente

---
+++ src/main/java/br/com/backend/course/resources/UserResource.java
@@ -0,0 +1,33 @@
+package br.com.backend.course.resources;
+
+import br.com.backend.course.entites.User;
+import br.com.backend.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findALL(){
+        List<User> List = service.findAll();
+        return ResponseEntity.ok().body(List);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/br/com/backend/course/services/UserService.java
@@ -0,0 +1,24 @@
+package br.com.backend.course.services;
+
+import br.com.backend.course.entites.User;
+import br.com.backend.course.repositores.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return  repository.findAll();
+    }
+    public User findById(Integer id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
